ee9a87e62311e324aa233ccb977bb8577748e30d
Stephane Recouvreur <stefan.recouvreur@gmail.com>
2016-01-26 23:10:16 +1100

    Create databse

--- flask-hello-world/env/app.py
+++ flask-hello-world/env/app.py
@@ -2,11 +2,43 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#error handling
+app.config["DEBUG"] = True
+
 @app.route("/")
 @app.route("/hello")
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print value + 1
+	return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+	print value + 1
+	return "correct"
+
+#dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+	print value
+	return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "michael":
+		return "Hello, {}".format(name), 200
+	else:
+		return "Not Found", 404
 
 def hello_world():
     return "Hello, World!"
 
 if __name__ == "__main__":
     app.run()
+
